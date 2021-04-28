620b6b73597524274350a249923ca4525195c5de
GeorgeBinDragon <GeorgeBinDragon@gmail.com>
2021-04-28 16:49:06 +0800

    example add baidu location

--- app_template_example/src/main/java/com/georgebindragon/app/template/example/MainActivity.java
+++ app_template_example/src/main/java/com/georgebindragon/app/template/example/MainActivity.java
@@ -1,16 +1,35 @@
 package com.georgebindragon.app.template.example;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import com.baidu.location.BDLocation;
+import com.baidu.location.LocationClientOption;
+import com.georgebindragon.lib.locate.baidu.BaiduLocationFunction;
+import com.georgebindragon.lib.locate.baidu.BaseBaiduMapLocationListener;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity
 {
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		BaiduLocationFunction.getInstance().init(this);
+		LocationClientOption locationClientOption = BaiduLocationFunction.getInstance().makeDefaultClientOption(10);
+		BaiduLocationFunction.getInstance().setOption(locationClientOption);
+		BaiduLocationFunction.getInstance().registerLocationListener(new BaseBaiduMapLocationListener()
+		{
+			@Override
+			public void onBDLocation(BDLocation bdLocation)
+			{
+				super.onBDLocation(bdLocation);
+			}
+		});
+
+
+		BaiduLocationFunction.getInstance().start();
 	}
 }

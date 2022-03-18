1d69da350d8b5f2ca05898dc92b3ed011ad8c39f
GeorgeBinDragon <georgebindragon@gmail.com>
2022-03-18 14:02:44 +0800

    project: add base lib module, and move base code into it.

---
+++ base_app/src/main/java/com/georgebindragon/base/utils/SoLibraryUtil.java
@@ -0,0 +1,30 @@
+package com.georgebindragon.base.utils;
+
+import android.content.Context;
+
+import com.georgebindragon.base.BaseUtils;
+import com.getkeepsafe.relinker.ReLinker;
+
+/**
+ * 创建人：George
+ *
+ * 描述：
+ *
+ * 修改：
+ */
+
+
+public class SoLibraryUtil
+{
+	private static final String TAG = "SoLibraryUtil-->";
+
+	public static void loadLibrary(final Context context, final String library)
+	{
+		ReLinker.loadLibrary(context, library);
+	}
+
+	public static void loadLibrary(final String library)
+	{
+		ReLinker.loadLibrary(BaseUtils.getContext(), library);
+	}
+}
